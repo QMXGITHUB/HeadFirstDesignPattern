187dde5331dc2525e367fbefd3ca9a89a111053c
QMXGITHUB <mxqiao@thoughtworks.com>
2016-10-26 14:47:32 +0800

    init status strategy practice project

--- StatusStratery/GumballMachine.cs
+++ StatusStratery/GumballMachine.cs
@@ -4,21 +4,14 @@ namespace StatusStratery
 {
     class GumballMachine
     {
-        int count;
-        static int SOLD_OUT = 0;
         static int NO_QUARTER = 1;
         static int HAS_QUARTER = 2;
-        static int SOLD = 3;
 
-        int state = SOLD_OUT;
+        int state;
 
-        public GumballMachine(int count)
+        public GumballMachine()
         {
-            this.count = count;
-            if (count > 0)
-            {
-                state = NO_QUARTER;
-            }
+            state = NO_QUARTER;
         }
 
         public string InsertQuarter()
@@ -29,14 +22,6 @@ namespace StatusStratery
             {
                 result = "You can't insert another quarter";
             }
-            else if (state == SOLD_OUT)
-            {
-                result = "You can't insert a quarter, the machine is sold out";
-            }
-            else if (state == SOLD)
-            {
-                result = "Please wait, we're already giving you a gumball";
-            }
             else if (state == NO_QUARTER)
             {
                 state = HAS_QUARTER;
@@ -46,91 +31,21 @@ namespace StatusStratery
             return result;
         }
 
-        public string EjectQuarter()
+        public string TurnCrank()
         {
             var result = default(string);
-            if (state == HAS_QUARTER)
-            {
-                result = "Quarter returned";
-                state = NO_QUARTER;
-            }
-            else if (state == NO_QUARTER)
-            {
-                result = "You haven't inserted a quarter";
-            }
-            else if (state == SOLD)
-            {
-                result = "Sorry, you already turned the crank";
-            }
-            else if (state == SOLD_OUT)
-            {
-                result = "You can't eject, you haven't insert a quarter yet";
-            }
 
-            return result;
-        }
-
-        public List<string> TurnCrank()
-        {
-            var result = new List<string>();
-
-            if (state == SOLD)
+            if (state == NO_QUARTER)
             {
-                result.Add("Turning twice doesn't get you another gumball");
-            }
-            else if (state == NO_QUARTER)
-            {
-                result.Add("You turned but there's no quarter");
-            }
-            else if (state == SOLD_OUT)
-            {
-                result.Add("You turned, but there are no gumballs");
+                result = "You turned but there's no quarter";
             }
             else if(state == HAS_QUARTER)
             {
-                result.Add("You turned...");
-                state = SOLD;
-                result.AddRange(Dispense());
-            }
-            return result;
-        }
-
-        private List<string> Dispense()
-        {
-            var result = new List<string>();
-            if (state == SOLD)
-            {
-                result.Add("A gumball comes rolling out the slot");
-                count = count - 1;
-                if (count == 0)
-                {
-                    result.Add("Oops, out of gumballs!");
-                    state = SOLD_OUT;
-                }
-                else
-                {
-                    state = NO_QUARTER;
-                }
-            }
-            else if (state == NO_QUARTER)
-            {
-                result.Add("You need to pay first");
-            }
-            else if (state == SOLD_OUT)
-            {
-                result.Add("No gumball dispensed");
-            }
-            else if (state == HAS_QUARTER)
-            {
-                result.Add("No gumball dispensed");
+                result = "You turned...A gumball comes rolling out the slot";
+                state = NO_QUARTER;
             }
 
             return result;
-        }
-
-        public int GetCount()
-        {
-            return count;
         }
     }
 }
--- StatusStratery/GumballMachineFact.cs
+++ StatusStratery/GumballMachineFact.cs
@@ -8,65 +8,20 @@ namespace StatusStratery
         [Fact]
         public void Should_roll_out_gumball_when_you_insert_a_quarter()
         {
-            var gumballMachine = new GumballMachine(5);
+            var gumballMachine = new GumballMachine();
 
             var results = gumballMachine.InsertQuarter();
             Assert.Equal("You inserted a quarter", results);
+
+            var insertQuarterTwice = gumballMachine.InsertQuarter();
+            Assert.Equal(insertQuarterTwice, "You can't insert another quarter");
+
             var turnCrank = gumballMachine.TurnCrank();
-            turnCrank.ShouldContain("You turned...");
-            turnCrank.ShouldContain("A gumball comes rolling out the slot");
-            int count = gumballMachine.GetCount();
-            Assert.Equal(4, count);
+            Assert.Equal(turnCrank,"You turned...A gumball comes rolling out the slot");
+
+            var crankTwice = gumballMachine.TurnCrank();
+            Assert.Equal("You turned but there's no quarter", crankTwice);
         }
-        
-        [Fact]
-        public void should_return_quarter_when_eject_quarter()
-        {
-            var gumballMachine = new GumballMachine(5);
-
-            gumballMachine.InsertQuarter();
-            var ejectQuarter = gumballMachine.EjectQuarter();
-            Assert.Equal("Quarter returned", ejectQuarter);
-            List<string> crank = gumballMachine.TurnCrank();
-            crank.ShouldContain("You turned but there's no quarter");
-            Assert.Equal(5, gumballMachine.GetCount());
-        }
-
-        [Fact]
-        public void should_return_you_have_not_insert_a_quarter_when_you_press_eject_quarter()
-        {
-            var gumballMachine = new GumballMachine(1);
-            var ejectQuarter = gumballMachine.EjectQuarter();
-            Assert.Equal("You haven't inserted a quarter", ejectQuarter);
-        }
-
-        [Fact]
-        public void should_not_insert_quarter_twice_continuously()
-        {
-            var gumballMachine = new GumballMachine(2);
-            var first = gumballMachine.InsertQuarter();
-            Assert.Equal("You inserted a quarter", first);
-            var second = gumballMachine.InsertQuarter();
-            Assert.Equal("You can't insert another quarter", second);
-        }
-
-        [Fact]
-        public void should_return_no_gumballs_when_there_is_no_gumballs()
-        {
-            var gumballMachine = new GumballMachine(1);
-            var first = gumballMachine.InsertQuarter();
-            var turnCrank = gumballMachine.TurnCrank();
-            turnCrank.ShouldContain("You turned...");
-            turnCrank.ShouldContain("A gumball comes rolling out the slot");
-            turnCrank.ShouldContain("Oops, out of gumballs!");
-
-            var second = gumballMachine.InsertQuarter();
-            Assert.Equal("You can't insert a quarter, the machine is sold out", second);
-
-            List<string> secondTurn = gumballMachine.TurnCrank();
-            secondTurn.ShouldContain("You turned, but there are no gumballs");
-        }
-
-
+       
     }
 }
