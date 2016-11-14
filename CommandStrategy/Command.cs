438effe8cda75eace79f20ff23a3790cdad28721
QMXGITHUB <mxqiao@thoughtworks.com>
2016-11-14 23:04:54 +0800

    init command strategy project

---
+++ CommandStrategy/Command.cs
@@ -0,0 +1,30 @@
+﻿namespace CommandStrategy
+{
+    public interface Command
+    {
+        void Execute();
+    }
+
+    public class LightOnCommand : Command
+    {
+        private Light light;
+
+        public LightOnCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            light.On();
+        }
+    }
+
+    public class Light
+    {
+        public void On()
+        {
+            
+        }
+    }
+}
