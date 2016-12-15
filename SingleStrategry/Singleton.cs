4be09feebdfb11279d20bc409cf07c0caa78b58a
QMXGITHUB <mxqiao@thoughtworks.com>
2016-12-15 22:57:04 +0800

    source code for single strategy.

---
+++ SingleStrategry/Singleton.cs
@@ -0,0 +1,20 @@
+﻿namespace SingleStrategry
+{
+    public class Singleton
+    {
+        private static Singleton uniqueInstance;
+
+        private Singleton()
+        {
+            
+        }
+        public static Singleton GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new Singleton();
+            }
+            return uniqueInstance;
+        }
+    }
+}
---
+++ SingleStrategry/SingletonWithMultiLine.cs
@@ -0,0 +1,24 @@
+﻿namespace SingleStrategry
+{
+    public class SingletonWithMultiLine
+    {
+        private volatile static SingletonWithMultiLine uniqueInstance = new SingletonWithMultiLine();
+        private static readonly object lockHelper = new object();
+        private SingletonWithMultiLine()
+        {
+            
+        }
+        public static SingletonWithMultiLine GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (lockHelper)
+                {
+                    if (uniqueInstance == null)
+                        uniqueInstance = new SingletonWithMultiLine();
+                }
+            }
+            return uniqueInstance;
+        }
+    }
+}
